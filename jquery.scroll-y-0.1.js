401a2e72ef733da7dd89b89e1ef3ff260e8f3b2e
Sean <dearseanharvey@gmail.com>
2013-05-30 12:09:00 -0700

    added option for fixed nav bar

--- jquery.scroll-y-0.1.js
+++ jquery.scroll-y-0.1.js
@@ -5,21 +5,30 @@
 * Date: 05/17/2013
 * Version: 0.1
 */
+
 (function($){
 
 $.fn.scrolly = function(options){
   // Setup defaults (more to come)
   var settings = $.extend({
-    blocks: $('.block')
+    stickyNav: true,
+    blocks: '.block',
+    duration: 800
   }, options);
 
   var $active;
   var $window = $(window);
-  var $page = $(document.body);
   var $nav = $(this);
+  var ogNavPos = $nav.offset().top - $window.scrollTop();
+  var $links = $nav.find('a');
   var $blocks = $(settings.blocks);
   // Handler each time the window scrolls
-  $window.on('scroll', function(){
+  $window.on('scroll', activeNav);
+  $window.on('scroll', stickyNav);
+  // Handler each time a user clicks on a nav <a>
+  $links.on('click', scrollToBlock);
+  // Function to figure out which nav item is currently in view
+  function activeNav(){
     var first = true;
     $blocks.each(function(i){ 
       // Setup top & bottom position variables for the current block in the loop
@@ -32,11 +41,11 @@ $.fn.scrolly = function(options){
         $nav.find('a.active').removeClass('active');
         $nav.find('a.' + $active.prev().attr('name')).addClass('active');
       } else {
-      	// Setup top & bottom position variables for the $active block in the loop
-      	var activeTopPos = $active.offset().top - ($window.scrollTop() + $nav.height());
-    	var activeBotPos = ($active.offset().top - $window.scrollTop() + $nav.height()) + $active.height();
-    	// If $active block's bottom edge is past the bottom of the nav or the current block in the loop
-    	// is less than or equal to the nav's bottom edge, move on
+        // Setup top & bottom position variables for the $active block in the loop
+        var activeTopPos = $active.offset().top - ($window.scrollTop() + $nav.height());
+        var activeBotPos = ($active.offset().top - $window.scrollTop() + $nav.height()) + $active.height()
+        // If $active block's bottom edge is past the bottom of the nav or the current block in the loop
+        // is less than or equal to the nav's bottom edge, move on
         if(activeBotPos < $nav.height() || blockTopPos <= $nav.height()){
           // If the current loop block's bottom edge is below the bottom edge of the nav and $active block's
           // bottom edge is past the nav's bottom edge set the current loop block to active and update it's 
@@ -49,7 +58,39 @@ $.fn.scrolly = function(options){
         }
       }
     });
-  });
+  };
+  // Scrolls to the section the user clicks
+  function scrollToBlock(){
+    // Grab the hash value for the target <a> and remove the '#'
+    var blockName = $(this).attr('href').replace('#','');
+    // Scroll down to the <a> anchor which corresponds to the item clicked 
+    // on in the nav
+    $('body, html').animate({
+      scrollTop: $('a[name='+ blockName +']').offset().top
+    }, settings.duration);
+    // Prevent default link behavior
+    return false;
+  };
+  // Sticky nav bar
+  function stickyNav(){
+    // Make sure we want the nav to stick to the top of the window
+    if(settings.stickyNav == true){
+      // If the top of the nav bar hits the top of the window change it's position to fixed
+      if($nav.offset().top - $window.scrollTop() <= 0){
+        $nav.css({
+          position:'fixed',
+          top: '0px'
+        });
+      };
+      // Puts the nav back into it's old place in the DOM once the nav reaches it's old position
+      if(ogNavPos > $window.scrollTop()){
+        $nav.css({
+          position:'relative',
+          top: 'inherit'
+        });
+      };
+    };
+  };
   return this;
 };
 
