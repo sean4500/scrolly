34073664670bfc0763064effbcbb58952158da6d
Sean Harvey <dearseanharvey@gmail.com>
2013-06-14 10:05:32 -0700

    fixed RegEx bug in Firefox

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-scrolly
+scroll-y
 =======
 
-Simple jQuery plugin which adds an active class to the nav for 1 page scroll only layout.
+Simple jQuery plugin which adds an active class to the nav for 1 page scroll type layout.
+
+Docs to come soon
--- jquery.scroll-y-0.1.js
+++ jquery.scroll-y-0.1.js
@@ -20,20 +20,33 @@ $.fn.scrolly = function(options){
   var $window = $(window);
   var $nav = $(this);
   var ogNavPos = $nav.offset().top - $window.scrollTop();
-  var $links = $nav.find('a');
+  var $links = $nav.find('li a');
   var $blocks = $(settings.blocks);
   // Handler each time the window scrolls
   $window.on('scroll', activeNav);
   $window.on('scroll', stickyNav);
   // Handler each time a user clicks on a nav <a>
   $links.on('click', scrollToBlock);
+  // Set active nav item when page loads
+  $window.on('load', activeNav);
   // Function to figure out which nav item is currently in view
   function activeNav(){
     var first = true;
+    // Grab the navs height
+    var navHeight = $nav.height();
+    // Add the top and bottom padding values to the overall height of the nav
+    navHeight += parseInt($nav.css('padding-top').replace(/[a-zA-Z]/g,''));
+    navHeight += parseInt($nav.css('padding-bottom').replace(/[a-zA-Z]/g,''));
+    // Loop through all the blocks
     $blocks.each(function(i){ 
+      // Get block height
+      var blockHeight = $(this).height();
+      // Account for any padding
+      blockHeight += parseInt($(this).css('padding-top').replace(/[a-zA-Z]/g,''));
+      blockHeight += parseInt($(this).css('padding-bottom').replace(/[a-zA-Z]/g,''));
       // Setup top & bottom position variables for the current block in the loop
-      var blockTopPos = $(this).offset().top - ($window.scrollTop() + $nav.height());
-      var blockBotPos = $(this).offset().top - ($window.scrollTop() + $nav.height()) + $(this).height();
+      var blockTopPos = $(this).offset().top - ($window.scrollTop() + navHeight);
+      var blockBotPos = $(this).offset().top - ($window.scrollTop() + navHeight) + blockHeight;
       // If we're on the first block set $active to the first block in the loop
       if(first == true){ 
         $active = $(this);
@@ -42,20 +55,20 @@ $.fn.scrolly = function(options){
         $nav.find('a.' + $active.prev().attr('name')).addClass('active');
       } else {
         // Setup top & bottom position variables for the $active block in the loop
-        var activeTopPos = $active.offset().top - ($window.scrollTop() + $nav.height());
-        var activeBotPos = ($active.offset().top - $window.scrollTop() + $nav.height()) + $active.height()
+        var activeTopPos = $active.offset().top - ($window.scrollTop() + navHeight);
+        var activeBotPos = ($active.offset().top - $window.scrollTop() - navHeight * 2) + $active.height()
         // If $active block's bottom edge is past the bottom of the nav or the current block in the loop
         // is less than or equal to the nav's bottom edge, move on
-        if(activeBotPos < $nav.height() || blockTopPos <= $nav.height()){
+        if(activeBotPos < navHeight || blockTopPos <= navHeight){
           // If the current loop block's bottom edge is below the bottom edge of the nav and $active block's
           // bottom edge is past the nav's bottom edge set the current loop block to active and update it's 
           // corresponding nav <a> with a class of "active"
-          if(blockBotPos > $nav.height() && activeBotPos <= $nav.height()){
+          if(blockBotPos > navHeight && activeBotPos <= navHeight){
             $active = $(this);
             $nav.find('a.active').removeClass('active');
             $nav.find('a.' + $active.prev().attr('name')).addClass('active');
           }
-        }
+        } 
       }
     });
   };
@@ -70,18 +83,22 @@ $.fn.scrolly = function(options){
       // Grab the navs height
       var navHeight = $nav.height();
       // Add the top and bottom padding values to the overall height of the nav
-      navHeight += parseInt($nav.css('padding-top').replace(/[^0-9]/g,''));
-      navHeight += parseInt($nav.css('padding-bottom').replace(/[^0-9]/g,''));
+      navHeight += parseInt($nav.css('padding-top').replace(/[a-zA-Z]/g,''));
+      navHeight += parseInt($nav.css('padding-bottom').replace(/[a-zA-Z]/g,''));
       // Set the scrollTop value to the appropriate block's anchor top offset minus the nav's height
-      var scrollTopVal = (ogNavPos > $window.scrollTop()) ? $('a[name='+ blockName +']').offset().top : $('a[name='+ blockName +']').offset().top;
+      var scrollTopVal = $('a[name='+ blockName +']').offset().top - navHeight;
     } else {
       // Otherwise if the nav is fixed just set the scrollTop to the top offset value of the block's anchor
       var scrollTopVal = $('a[name='+ blockName +']').offset().top;
-    }
+    };
     // Scroll down to the <a> anchor which corresponds to the item clicked on in the nav
-    $('body, html').animate({
-      scrollTop: scrollTopVal
-    }, settings.duration);
+    if($window.width() > 768){
+      $('body, html').animate({
+        scrollTop: scrollTopVal
+      }, settings.duration);
+    } else {
+      $('body, html').scrollTop(scrollTopVal);
+    };
     // Prevent default link behavior
     return false;
   };
@@ -92,14 +109,16 @@ $.fn.scrolly = function(options){
       // Grab the navs height
       var navHeight = $nav.height();
       // Add the top and bottom padding values to the overall height of the nav
-      navHeight += parseInt($nav.css('padding-top').replace(/[^0-9]/g,''));
-      navHeight += parseInt($nav.css('padding-bottom').replace(/[^0-9]/g,''));
+      navHeight += parseFloat($nav.css('padding-top').replace(/[a-zA-Z]/g,''));
+      navHeight += parseFloat($nav.css('padding-bottom').replace(/[a-zA-Z]/g,''));
       // If the top of the nav bar hits the top of the window change it's position to fixed and give the body
       // the amount of padding equal to the height of the nav so the page doesn't jump
       if($nav.offset().top - $window.scrollTop() <= 0){
         $nav.css({
           position:'fixed',
-          top: '0px'
+          top: '0',
+          width: '100%',
+          'z-index': '700'
         });
         $('body').css({
           paddingTop: navHeight + 'px'
